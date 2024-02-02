e2fb02f3c7a4d375e3b37bb01fd08f120e11f6a6
Muhammad Sohail <smartsohail580.com>
2024-02-02 19:07:11 +0500

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Card from './card'
+
+function App() {
+  return (
+    <div className='flex justify-center gap-2'>
+      <Card name='Shehzadi' button='Visit Profile'/>
+      <Card name='Alia' button='here Profile'/>
+      <Card name='Smart' button='Click Profile'/>
+    </div>
+  )
+}
+
+export default App
